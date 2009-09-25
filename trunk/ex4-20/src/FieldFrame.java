c6f26c8599b1adf697bb4d50d23b04802e24dfa2
cryptodecker <cryptodecker@6511626a-9b1e-11de-96bd-0376b6bc6ad2>
2009-09-25 18:34:15 +0000

    Saving data. Not functional Code.

--- trunk/ex4-20/src/FieldFrame.java
+++ trunk/ex4-20/src/FieldFrame.java
@@ -13,35 +13,55 @@ import javax.swing.JButton;
 
 public class FieldFrame extends JFrame implements ActionListener
 {
-	JTextField txtLName, txtFName, txtHrRate, txtEmployeeNum;;
-	JButton butSubmit;
-	JTable table;
-	String[] colHeadings={ "Employee Number" , "Last Name" , "First Name" , "Hourly Rate" };
-	EmployeeData[] employeeData;
+	private JPanel inputPanel;
+	private BorderLayout borderLayout1;
+	private JTextField txtLName, txtFName, txtHrRate, txtEmployeeNum;
+	private JButton butSubmit;
+	private JTable table;
+	private String[] colHeadings={ "Employee Number" , "Last Name" , "First Name" , "Hourly Rate" };
+	private EmployeeData[] employeeData;
 	
 	//TextFieldConstructor******************
 	public FieldFrame()
 	{
-		super("Testing Buttons");
-		employeeData = EmployeeDB.getEmployeeData();
-		setLayout( new FlowLayout());
+		super("Employee Calculations");
+		inputPanel = new JPanel();
+		inputPanel.setLayout (new GridLayout(4,1));
+		borderLayout1 = new BorderLayout(3,3);
+		setLayout( borderLayout1);
 		txtLName = new JTextField("Enter Surname Here");
 		txtFName = new JTextField("Enter First Name Here");
 		txtHrRate = new JTextField("Hourly Rate in USD");
 		butSubmit = new JButton("Submit");
 		//table = new JTable( EmployeeDB.employeeData,colHeadings );	
-		butSubmit.addActionListener(this);
-		add( txtLName); add(txtFName); add(txtHrRate);add(butSubmit);
-		//construct txtField
-	}//end method TxtFieldFrame
+		inputPanel.add( txtLName, BorderLayout.EAST);
+		inputPanel.add(txtFName, BorderLayout.EAST); 
+		inputPanel.add(txtHrRate,BorderLayout.EAST);
+		inputPanel.add(butSubmit,BorderLayout.EAST);
+		add(inputPanel, BorderLayout.EAST);
+		butSubmit.addActionListener(
+				//Process Submit Button event
+				new ActionListener()//anonymous inner class
+				{
+					public void actionPerformed(ActionEvent ae)
+					{
+						for (employeeData.employeeNum : employeeData[])
+						{
+							/**
+							 * txtLPhoto = lPhoto.getText();
+			txtWPhoto = wPhoto.getText();
+			int intLPhoto = Integer.parseInt(txtLPhoto.trim());
+			int intWPhoto = Integer.parseInt(txtWPhoto.trim());
+			CostOfFraming()
+							 */
+						}
+						
+							get
+						};
+						JOptionPane.showMessageDialog(FieldFrame.this, "Submit test complete.");
+					}//end method actionPerformed
+				}//end anonymous innerclass for submit button
+		);//end call to addaCTIONlISTNER
+	}//end Method fieldFrame
 	
-	//inner class for button event handling
-	public void actionPerformed (ActionEvent event)
-	{
-		Object obj = event.getSource();
-		if (obj == butSubmit)
-		{
-			System.out.printf("Great");
-		}//end If for butSubmit Click
-	}//end method actionPerformed
 }//End class FieldFrame

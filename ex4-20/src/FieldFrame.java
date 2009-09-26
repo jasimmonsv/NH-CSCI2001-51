1fff9dbe052cb9a115f142187f5d0ee4de6da102
cryptodecker <cryptodecker@6511626a-9b1e-11de-96bd-0376b6bc6ad2>
2009-09-26 02:38:07 +0000

    Code is mostly working. Will not repaint the JTable to reflect added users.

--- ex4-20/src/FieldFrame.java
+++ ex4-20/src/FieldFrame.java
@@ -10,58 +10,95 @@ import javax.swing.event.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
+import javax.swing.table.AbstractTableModel;
 
 public class FieldFrame extends JFrame implements ActionListener
 {
 	private JPanel inputPanel;
 	private BorderLayout borderLayout1;
-	private JTextField txtLName, txtFName, txtHrRate, txtEmployeeNum;
+	private JTextField txtLName, txtFName, txtHrRate;
 	private JButton butSubmit;
-	private JTable table;
+	private JLabel lblLName, lblFName, lblHrRate;
+	private EmployeeDB employeeDB;
+	private JTable emplTable;
+	private String[][] employeeData;
 	private String[] colHeadings={ "Employee Number" , "Last Name" , "First Name" , "Hourly Rate" };
-	private EmployeeData[] employeeData;
 	
 	//TextFieldConstructor******************
 	public FieldFrame()
 	{
 		super("Employee Calculations");
+		//setup panels and general layout of the screen.
 		inputPanel = new JPanel();
 		inputPanel.setLayout (new GridLayout(4,1));
 		borderLayout1 = new BorderLayout(3,3);
 		setLayout( borderLayout1);
-		txtLName = new JTextField("Enter Surname Here");
-		txtFName = new JTextField("Enter First Name Here");
-		txtHrRate = new JTextField("Hourly Rate in USD");
+		
+		//setup components
+		employeeDB = new EmployeeDB();
+		txtLName = new JTextField();
+		txtLName.setBorder(BorderFactory.createLineBorder(Color.black));
+		txtFName = new JTextField();
+		txtFName.setBorder(BorderFactory.createLineBorder(Color.black));
+		txtHrRate = new JTextField();
+		txtHrRate.setBorder(BorderFactory.createLineBorder(Color.black));
 		butSubmit = new JButton("Submit");
-		//table = new JTable( EmployeeDB.employeeData,colHeadings );	
-		inputPanel.add( txtLName, BorderLayout.EAST);
-		inputPanel.add(txtFName, BorderLayout.EAST); 
-		inputPanel.add(txtHrRate,BorderLayout.EAST);
-		inputPanel.add(butSubmit,BorderLayout.EAST);
+		lblFName = new JLabel("Enter first name: ");
+		lblHrRate = new JLabel("Enter hourly rate in USD:");
+		lblLName = new JLabel("Enter surname:");
+		employeeData = new String[EmployeeDB.getCustomerCount()-1][4];
+		employeeData=EmployeeDB.getEmployeeData();
+		emplTable = new JTable( employeeData,colHeadings );	
+
+		//add components to the proper panel
+		inputPanel.add(lblLName, BorderLayout.EAST );
+		inputPanel.add( txtLName, BorderLayout.EAST );
+		inputPanel.add( lblFName, BorderLayout.EAST );
+		inputPanel.add( txtFName, BorderLayout.EAST );
+		inputPanel.add( lblHrRate, BorderLayout.EAST );
+		inputPanel.add( txtHrRate,BorderLayout.EAST );
+		inputPanel.add( butSubmit,BorderLayout.EAST );
+		add( emplTable, BorderLayout.CENTER );
+		
+		//Add inputPanel into main JPanel
 		add(inputPanel, BorderLayout.EAST);
+		
+		//Process Actions and Events
 		butSubmit.addActionListener(
 				//Process Submit Button event
 				new ActionListener()//anonymous inner class
 				{
 					public void actionPerformed(ActionEvent ae)
 					{
-						for (employeeData.employeeNum : employeeData[])
+						String lName = txtLName.getText();
+						String fName = txtFName.getText();
+						String hrRate = txtHrRate.getText();
+						double dblHrRate;
+						dblHrRate = Double.parseDouble(hrRate);
+						if (lName.equals("") || fName.equals("") || hrRate.equals(""))//if informaion is missing, throw error and drop out.
 						{
-							/**
-							 * txtLPhoto = lPhoto.getText();
-			txtWPhoto = wPhoto.getText();
-			int intLPhoto = Integer.parseInt(txtLPhoto.trim());
-			int intWPhoto = Integer.parseInt(txtWPhoto.trim());
-			CostOfFraming()
-							 */
-						}
-						
-							get
-						};
-						JOptionPane.showMessageDialog(FieldFrame.this, "Submit test complete.");
+							JOptionPane.showMessageDialog(FieldFrame.this, "Error! Please enter all information");
+							return;
+						}//endif
+						EmployeeDB.addEmployee(lName,fName,dblHrRate);
+						JOptionPane.showMessageDialog(FieldFrame.this, "User Added Successfully");
+						//After successful adding, clear prior information.
+						txtLName.setText("");
+						txtFName.setText("");
+						txtHrRate.setText("");
+						lName="";
+						fName="";
+						hrRate="";
+						dblHrRate=0;
+						employeeData=EmployeeDB.getEmployeeData();
 					}//end method actionPerformed
 				}//end anonymous innerclass for submit button
-		);//end call to addaCTIONlISTNER
+		);//end call to butSubmit.addActionListner
 	}//end Method fieldFrame
+	
+	public void displayEmployeeData()
+	{
+		
+	}//end method displayEmployeeData
 	
 }//End class FieldFrame
--- ex4-20/src/SalaryCalc.java
+++ ex4-20/src/SalaryCalc.java
@@ -7,11 +7,14 @@ import javax.swing.JFrame;
 
 public class SalaryCalc 
 {
+	//Declare global variables
+			
 	public static void main(String[] args) 
 	{
 		FieldFrame fieldFrame = new FieldFrame();
 		fieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		fieldFrame.setSize(800,600);
 		fieldFrame.setVisible( true );
+		
 	}//end Method Main
 }//end class SalaryCalc

b5f7d41782fb56ab8d0c9ee2927b19dc62ecd8f6
cryptodecker <cryptodecker@6511626a-9b1e-11de-96bd-0376b6bc6ad2>
2009-09-26 02:38:07 +0000

    Code is mostly working. Will not repaint the JTable to reflect added users.

--- trunk/ex4-20/src/EmployeeDB.java
+++ trunk/ex4-20/src/EmployeeDB.java
@@ -5,23 +5,48 @@
  * This class is the structure for the Employee data and any other needed methods
  */
 
+import java.util.*;
+
 public class EmployeeDB 
 {
-	private static EmployeeData[] employeeData; //array of Employee Data
+	private static ArrayList<EmployeeData> employeeData; //arrayList of Employee Data
 	
 	//no Argument employeeDB constructor for testing
 	public EmployeeDB()
 	{
-		employeeData = new EmployeeData[2]; // 2 default accounts for testing
-		employeeData[0] = new EmployeeData(001, "Doe","John",25.0);
-		employeeData[1] = new EmployeeData(002, "Public","John",10.0);
+		employeeData = new ArrayList<EmployeeData>(); // initalizing ArrayList of EmployeeData
+		employeeData.add(new EmployeeData(001, "Doe","John",25.0));
+		employeeData.add(new EmployeeData(002, "Public","John",10.0));
 	}//end Constructor
-	
-	public EmployeeData[] getEmployeeData()
+		
+	//method ot add another employee
+	public static void addEmployee(String lName, String fName, double hrRate)
 	{
-		return employeeData;
+		int employeeNum = employeeData.size();
+		employeeData.add(new EmployeeData(employeeNum,lName,fName,hrRate));
+		return;
+	}//end method addEmployee
+	
+	//Method to grab total number of customers
+	public static int getCustomerCount()
+	{
+		return employeeData.size();
+	}//end method getCustomerCount
+	
+	//convert all employeedata to a 2D array for printing
+	public static String[][] getEmployeeData()
+	{
+		String data[][];
+		data = new String[employeeData.size()][4];
+		//for loop to transfer data to 2d array
+		for (int i=0; i<=(employeeData.size()-1);i++)
+		{
+			data[i][0]=employeeData.get(i).getLastName();
+			data[i][1]=employeeData.get(i).getFirstName();
+			data[i][2]=Double.toString(employeeData.get(i).getHrRate());
+		}//end for loop
+		
+		return data;
 	}//end method getEmployeeData
-	
-	
 
 }

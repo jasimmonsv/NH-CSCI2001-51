d8ae3d23b59c9359cf2f8dfe18357bf861502717
cryptodecker <cryptodecker@6511626a-9b1e-11de-96bd-0376b6bc6ad2>
2009-10-14 22:22:40 +0000

    Incomplete - taskLanding Page

--- trunk/ex4-20/src/FieldFrame.java
+++ trunk/ex4-20/src/FieldFrame.java
@@ -4,58 +4,76 @@
  * @date Sep 24, 2009
  */
 import java.awt.*;
-import javax.swing.JOptionPane;
 import javax.swing.*;
-import javax.swing.event.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JButton;
+//import javax.swing.event.*;
+import java.awt.event.*;
 import javax.swing.table.AbstractTableModel;
+
+/*
+ * TODO Setup read/write to backup file
+ */
 
 public class FieldFrame extends JFrame implements ActionListener
 {
-	private JPanel inputPanel, dispPanel;
-	private BorderLayout borderLayout1;
-	private JTextField txtLName, txtFName, txtHrRate;
-	private JButton butSubmit,hrsWorked;
-	private JLabel lblLName, lblFName, lblHrRate;
-	private EmployeeDB employeeDB;
-	private JTable emplTable;
-	private String[][] employeeData;
-	private String[] colHeadings={ "Employee Number" , "Last Name" , "First Name" , "Hourly Rate", "Weekly Pay" };
+	private JPanel inputPanel, employeeDataPanel;//display panels
+	private JTextField txtLName, txtFName, txtHrRate;//txt fields for user input
+	private JButton butSubmit,hrsWorked; //action buttons
+	private JLabel lblLName, lblFName, lblHrRate;//labels for user input
+	private EmployeeDB employeeDB;//need this DB for referencing
+	private static String[][] employeeData; //store employeeData
+	private ButtonHandler handler;//button Action handler
+	private String[] colHeadings={ "Employee Number" , "Last Name" , "First Name" , "Hourly Rate", "Weekly Pay" };//headings for 2d array
 	
 	//TextFieldConstructor******************
 	public FieldFrame()
 	{
 		super("Employee Calculations");
 		//setup panels and general layout of the screen.
-		inputPanel = new JPanel();
-		inputPanel.setLayout (new GridLayout(4,1));
-		dispPanel = new JPanel();
-		dispPanel.setLayout(new FlowLayout());
-		borderLayout1 = new BorderLayout(3,3);
-		setLayout( borderLayout1);
+		handler = new ButtonHandler();
+		employeeDB = new EmployeeDB(); //setup employeeDB for reference
+		//setup reference data
+		employeeData = new String[EmployeeDB.getCustomerCount()-1][4];
+		employeeData=EmployeeDB.getEmployeeData();
 		
-		//setup components
-		employeeDB = new EmployeeDB();
+		//call methods to setup Panels
+		setupInputPanel();
+		employeeDisplaySetup();
+				
+		//Add inputPanel into main JPanel
+		add( inputPanel, BorderLayout.EAST );
+		add( employeeDataPanel, BorderLayout.CENTER );
+		
+	}//end Constructor fieldFrame*/
+	
+	//this methods initalizes components that will be in the inputPanel
+	private void setupInputPanel()
+	{
+		//initalize inputPanel
+		inputPanel = new JPanel();	
+		inputPanel.setLayout (new GridLayout(4,1));
+		
+		//***initalize components***
+		//setup textFields
 		txtLName = new JTextField();
 		txtLName.setBorder(BorderFactory.createLineBorder(Color.black));
 		txtFName = new JTextField();
 		txtFName.setBorder(BorderFactory.createLineBorder(Color.black));
 		txtHrRate = new JTextField();
 		txtHrRate.setBorder(BorderFactory.createLineBorder(Color.black));
-		butSubmit = new JButton("Submit");
-		hrsWorked = new JButton("Submit TimeSheet");
+		
+		//setup labels
 		lblFName = new JLabel("Enter first name: ");
 		lblHrRate = new JLabel("Enter hourly rate in USD:");
 		lblLName = new JLabel("Enter surname:");
-		employeeData = new String[EmployeeDB.getCustomerCount()-1][4];
-		employeeData=EmployeeDB.getEmployeeData();
-		emplTable = new JTable( employeeData,colHeadings );	
-		emplTable.setPreferredScrollableViewportSize( new Dimension(200,100));
-
+		
+		//setup buttons
+		butSubmit = new JButton("Submit");
+		hrsWorked = new JButton("Submit TimeSheet");
+		butSubmit.addActionListener(handler);//end call to butSubmit.addActionListner
+		hrsWorked.addActionListener(handler);//end call to hrsWorked.addActionListner*/
+		
 		//add components to the input panel
-		inputPanel.add(lblLName, BorderLayout.EAST );
+		inputPanel.add( lblLName, BorderLayout.EAST );
 		inputPanel.add( txtLName, BorderLayout.EAST );
 		inputPanel.add( lblFName, BorderLayout.EAST );
 		inputPanel.add( txtFName, BorderLayout.EAST );
@@ -64,20 +82,47 @@ public class FieldFrame extends JFrame implements ActionListener
 		inputPanel.add( butSubmit,BorderLayout.EAST );
 		inputPanel.add( hrsWorked,BorderLayout.EAST );
 		
-		//add components to the dispPanel
-		dispPanel.add(emplTable);
-		//Add inputPanel into main JPanel
-		add( inputPanel, BorderLayout.EAST );
-		add( dispPanel, BorderLayout.WEST );
 		
-		//Process Actions and Events
-		butSubmit.addActionListener(
-				//Process Submit Button event
-				new ActionListener()//anonymous inner class
-				{
-					public void actionPerformed(ActionEvent ae)
-					{
-						String lName = txtLName.getText();
+	}//end method setupInputPanel
+	
+	//setup EmployeeDisplay table
+	private void employeeDisplaySetup()
+	{
+		employeeDataPanel = new JPanel();
+		employeeDataPanel.setLayout(new GridLayout());
+		/*
+		 * TODO  populate employeeDataPanel with existing employee data
+		 */
+	}//end method employeDisplaySetup */
+	
+	public void reDisplayEmployeeData()
+	{
+		/*
+		 * TODO re-engineer employeeDataRedraw
+		 */
+		employeeData = new String[EmployeeDB.getCustomerCount()-1][4];
+		employeeData=EmployeeDB.getEmployeeData();
+		//emplTable.removeAll();
+		//emplTable= new JTable( employeeData,colHeadings );	
+		scrollPane.removeAll();
+		scrollPane.add(emplTable);
+		add( scrollPane, BorderLayout.CENTER );
+		//validate and repaint
+		emplTable.validate();
+		scrollPane.validate();
+		emplTable.repaint();
+		scrollPane.repaint();
+	}//end method displayEmployeeData*/
+	
+	//********************ButtonHandler********************
+	private class ButtonHandler implements ActionListener
+    {
+            public void  actionPerformed (ActionEvent ae)
+            {
+                    Object obj = ae.getSource();
+                    if (obj == butSubmit)
+                    {
+                    	String lName = txtLName.getText();
 						String fName = txtFName.getText();
 						String hrRate = txtHrRate.getText();
 						double dblHrRate;
@@ -97,32 +142,18 @@ public class FieldFrame extends JFrame implements ActionListener
 						fName="";
 						hrRate="";
 						dblHrRate=0;
-						displayEmployeeData();
-					}//end method actionPerformed
-				}//end anonymous innerclass for submit button
-		);//end call to butSubmit.addActionListner
-		
-		hrsWorked.addActionListener(
-				//Process Submit Button event
-				new ActionListener()//anonymous inner class
-				{
-					public void actionPerformed(ActionEvent ae)
-					{
-						int empIDNum = Integer.parseInt(JOptionPane.showInputDialog("Enter Employee ID Number: "));
+						reDisplayEmployeeData();
+                    }//end butSubmit if statement
+                    
+                    if (obj == hrsWorked)
+                    {
+                    	int empIDNum = Integer.parseInt(JOptionPane.showInputDialog("Enter Employee ID Number: "));
 				        double empWorkHrs = Double.parseDouble(JOptionPane.showInputDialog("Enter Hours Worked: "));
 				        double weekPay = EmployeeDB.getWeeklyPay(empIDNum,empWorkHrs);
 				        JOptionPane.showMessageDialog(null,"Weekly Pay is: $"+ weekPay);
-					}//end method actionPerformed
-				}//end anonymous innerclass for hrsWorked button
-		);//end call to hrsWorked.addActionListner
-		
-	}//end Method fieldFrame
+                    }//end hrsWorked if statement
+                    
+            }//end private method actionPerformed
+    }//end private class ButtonHandler	
 	
-	public void displayEmployeeData()
-	{
-		employeeData=EmployeeDB.getEmployeeData();
-		inputPanel.repaint();
-		dispPanel.validate();
-	}//end method displayEmployeeData
-	
-}//End class FieldFrame
+}//End class FieldFrame*/

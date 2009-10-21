57378ac4ad761b322f8c94fdad95457fa8899348
cryptodecker <cryptodecker@6511626a-9b1e-11de-96bd-0376b6bc6ad2>
2009-10-21 17:29:52 +0000

    v0.1 Initial Code

---
+++ AirlineTicketSystem/src/AirlineTicketSystem.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @author J.A. Simmons V
+ * @version 0.1 21Oct2009
+ * 
+ */
+import javax.swing.JFrame;
+
+public class AirlineTicketSystem 
+{
+
+	/**
+	 * @param args
+	 */	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LabelFrame labelFrame = new LabelFrame();//create LabelFrame
+		labelFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+		labelFrame.setSize(800,600); //set frame size
+		labelFrame.setVisible( true ); //display frame
+
+	}//end main Method */
+
+}//end AirLineTicketSystem Class */

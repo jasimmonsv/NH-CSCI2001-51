7fc04864c433619d83f1c913597c58ff5562b566
cryptodecker <cryptodecker@6511626a-9b1e-11de-96bd-0376b6bc6ad2>
2009-11-17 21:08:22 +0000

    Incomplete - taskLanding Page

---
+++ trunk/RollDice/src/MainFrame.java
@@ -0,0 +1,83 @@
+import java.awt.*;
+import javax.swing.*;
+
+import sun.audio.AudioPlayer;
+
+import java.awt.event.*;
+
+
+public class MainFrame extends JFrame
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7145892307744911073L;
+	private JLabel lblResult;
+	private ButtonHandler handler;
+	JPanel pnlCenterPanel, pnlDiceRoll;
+	private JButton btnRoll;
+	private Dice dice;
+	
+	//Constructor
+	public MainFrame()
+	{
+	super("Dice Roll");
+	handler = new ButtonHandler();
+	setLayout( new BorderLayout() );//setBorderLayout
+	
+	//initalize components
+	dice = new Dice();
+	pnlCenterPanel = new JPanel();
+	pnlCenterPanel.setLayout( new BorderLayout() );
+	pnlDiceRoll = new JPanel();
+	pnlDiceRoll.setLayout( new GridLayout() );
+	JLabel lblTitle = new JLabel("Dice Roll", JLabel.CENTER);
+	lblResult = new JLabel("2", JLabel.CENTER);
+	btnRoll = new JButton("Roll!");
+	btnRoll.addActionListener(handler);
+	
+	//add components to frame and Panels
+	add( lblTitle, BorderLayout.NORTH );
+	add( btnRoll, BorderLayout.SOUTH );
+	add( pnlCenterPanel, BorderLayout.CENTER );
+	pnlCenterPanel.add( lblResult, BorderLayout.SOUTH );
+	pnlCenterPanel.add( pnlDiceRoll, BorderLayout.CENTER );
+	pnlDiceRoll.add( new JLabel( dice.getFace( 1 ) ) );
+	pnlDiceRoll.add( new JLabel( dice.getFace( 1 ) ) );
+	
+	}//end Constructor
+	
+	private void rollDice()
+	{
+		//initialize	
+		int dice1 = dice.rollDice();
+		int dice2 = dice.rollDice();
+		//remove all from panels
+		pnlDiceRoll.removeAll();
+		//rebuild
+		dice.getFace(dice1);
+		dice.getFace(dice2);
+		lblResult.setText(Integer.toString(dice1+dice2));
+		pnlDiceRoll.add( new JLabel( dice.getFace( dice1 ) ) );
+		pnlDiceRoll.add( new JLabel( dice.getFace( dice2 ) ) );
+		//validate and repaint
+		pnlDiceRoll.validate();
+		pnlDiceRoll.repaint();
+	}//end method rollDice */
+	
+	private class ButtonHandler implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent ae) 
+		{
+			Object obj = ae.getSource();
+			if (obj == btnRoll)
+				AudioPlayer.player.start(dice.getSound(0));
+				rollDice();		
+			
+		}//end method actionPerformed */
+		
+	}//end anonymous class ButtonHandler */
+
+}//end Class MainFrame
---
+++ trunk/RollDice/src/RollDice.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+
+public class RollDice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		MainFrame frame1 = new MainFrame();
+		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame1.setSize( 400, 300);
+		frame1.setVisible(true);
+
+	}
+
+}

f949eaa033acb1727cdd513ea0824351ae9ec1b3
cryptodecker <cryptodecker@6511626a-9b1e-11de-96bd-0376b6bc6ad2>
2009-11-18 04:38:40 +0000

    Incomplete - taskLanding Page

--- trunk/RollDice/src/Dice.java
+++ trunk/RollDice/src/Dice.java
@@ -1,4 +1,10 @@
-import java.applet.AudioClip;
+/**
+ * @author J.A. Simmons V
+ * @version 1.0 17Nov2009
+ * 
+ * This is a class to build a single die and all the methods needed for it.
+ */
+
 import java.util.Random;
 import javax.swing.*;
 import sun.audio.*;
@@ -9,11 +15,10 @@ public class Dice
 	//Constructor
 	public Dice()
 	{
-		int[] diceNum;
-		ImageIcon[] diceFace;
 		
 	}//end constructor */
 	
+	//method that will return a random number between 1 and 6
 	public int rollDice()
 	{
 		int face;
@@ -22,6 +27,8 @@ public class Dice
 		return face;		
 	}//end method rollDice */
 	
+	//method that will accept an integer and return an image icon coorsponding to 
+	//that integer
 	public ImageIcon getFace(int diceNum)
 	{
 		ImageIcon diceFace;
@@ -36,13 +43,15 @@ public class Dice
 		return diceFace;
 	}//end method diceFace */
 	
+	//method that will play the sound of dice rolling and a dissapointed sound
+	//for when a 7 is rolled
 	public AudioStream getSound(int clipNum) throws IOException
 	{
-		//TODO add try block
-		try{
-		InputStream in1 = new FileInputStream("./diceRoll.wav");
-		InputStream in2 = new FileInputStream("./disappointment.wav");
-		}catch{}
+		
+		/*InputStream in1 = new FileInputStream("diceRoll.au");
+		InputStream in2 = new FileInputStream("disappointment.wav");*/
+		InputStream in1 = new FileInputStream("C:\\temp\\diceRoll.au");
+		InputStream in2 = new FileInputStream("C:\\temp\\disappointment.wav");
 		AudioStream[] sound = new AudioStream[2];
 		sound[0] = new AudioStream(in1);
 		sound[1] = new AudioStream(in2);
